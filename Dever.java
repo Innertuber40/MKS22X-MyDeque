9d8fc5e816f96e128a4195d00e2ef0fd1e48e169
bstringham <bstringham@stuy.edu>
2019-03-22 10:41:53 -0400

    Successfully created the add command

--- Dever.java
+++ Dever.java
@@ -1,5 +1,13 @@
 public class Dever {
 	public static void main(String[] args) {
 		MyDeque<Integer> nDegree = new MyDeque<Integer>();
+		nDegree.addFirst(6);
+		nDegree.addFirst(-678);
+		nDegree.addFirst(112);
+		nDegree.addFirst(345);
+		nDegree.addFirst(0);
+		nDegree.addFirst(-6);
+		System.out.println("-6, 0, 345, 112, -678, 6");
+		System.out.println(nDegree);
 	}
 }
--- MyDeque.java
+++ MyDeque.java
@@ -4,11 +4,11 @@ public class MyDeque<E> {
 	private int start;
 	private int end;
 
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public MyDeque() {
 		deque = (E[])new Object[10];
 	}
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public MyDeque(int initialCapacity) {
 		deque = (E[])new Object[initialCapacity];
 	}
@@ -20,36 +20,49 @@ public class MyDeque<E> {
 		String returns = "";
 		if (start < end) {
 			for (int i = start; i < end; i++) {
-				returns += deque[i];
+				returns += deque[i] + " ";
 			}
 		} else {
 			for (int i = start; i < deque.length; i++) {
-				returns += deque[i];
+				returns += deque[i] + " ";
 			}
 			for (int i = 0; i < end; i++) {
-				returns += deque[i];
+				returns += deque[i] + " ";
 			}
 		}
 		return returns;
 	}
 
 	@SuppressWarnings("unchecked")
-	public void addFirst(E element){
+	public void addFirst(E element) {
 		if (start == end) {
-			newArray = (E[]) new Object[deque.length * 2];
+			E[] newArray = (E[])new Object[deque.length * 2];
+			int j = 0;
 			if (start < end) {
 				for (int i = start; i < end; i++) {
-					returns += deque[i];
+					newArray[j] = deque[i];
+					j++;
 				}
 			} else {
 				for (int i = start; i < deque.length; i++) {
-					returns += deque[i];
+					newArray[j] = deque[i];
+					j++;
 				}
 				for (int i = 0; i < end; i++) {
-					returns += deque[i];
+					newArray[j] = deque[i];
+					j++;
 				}
 			}
-		}	
+			deque = newArray;
+		}
+		start--;
+		if (start == -1) {
+			deque[deque.length - 1] = element;
+			start = deque.length - 1;
+		} else {
+			deque[start] = element;
+		}
+		size++;
 	}
 	@SuppressWarnings("unchecked")
 	public void addLast(E element){
@@ -57,16 +70,16 @@ public class MyDeque<E> {
 	}
 	
 	public E removeFirst(){
-	
+		return null;	
 	}
 	public E removeLast(){
-	
+		return null;
 	}
 	
 	public E getFirst(){
-	
+		return null;
 	}
  	public E getLast(){
-	
+		return null;
 	}
 }
